c3009e48e1957dad8104d3c973b9c159b8ce2e39
Vladislav Kotsev <vladygk8@gmaila.com>
2023-08-16 19:31:57 +0300

    Add boostrap

--- ProfileCardCreator/ProfileCardCreatorApp/urls.py
+++ ProfileCardCreator/ProfileCardCreatorApp/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from ProfileCardCreator.ProfileCardCreatorApp.views import IndexView
 
-urlpatterns = (path("", IndexView.as_view(), name="index"),)
+urlpatterns = (path("", IndexView.as_view(), name="home page"),)
