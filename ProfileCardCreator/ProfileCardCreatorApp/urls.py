72ce5d662a79a4310fa69b87b70f62be61da92c2
Vladislav Kotsev <vladygk8@gmaila.com>
2023-08-17 01:47:50 +0300

    restart app

--- ProfileCardCreator/ProfileCardCreatorApp/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-from ProfileCardCreator.ProfileCardCreatorApp.views import IndexView
-
-urlpatterns = (path("", IndexView.as_view(), name="home page"),)
--- ProfileCardCreator/ProfileCardCreatorApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views import generic as views
-# Create your views here.
-
-
-class IndexView(views.TemplateView):
-    template_name = "index.html"
